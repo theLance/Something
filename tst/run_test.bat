c66f230df5072954b0ae03a4467b5cbae62375d1
Lance <lance.alter@gmail.com>
2012-12-18 20:16:17 +0100

    cosmetics

--- tst/run_test.bat
+++ tst/run_test.bat
@@ -1,6 +1,6 @@
 python c:\mingw\cxxtest\bin\cxxtestgen --error-printer -o runner.cpp test.hpp
 
-g++  runner.cpp -Lc:\BOOST\boost_1_52_0\stage\lib\ -std=c++0x^
+g++  runner.cpp -std=c++0x -Lc:\BOOST\boost_1_52_0\stage\lib\^
 	^
 	-lboost_date_time-mgw46-mt-1_52 ^
 	-lboost_filesystem-mgw46-mt-1_52 ^
